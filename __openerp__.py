2f3c3be47a02fddd5d5b25eb54a27183c8c3381e
Manuel Marquez <buzondemam@gmail.com>
2017-10-30 05:43:35 -0400

    [ADD] Field "Purchase orders expiration limit" in settings of company

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Manager of Purchase Orders Expired',
+    'version': '9.0.0.1.0',
+    'category': 'Purchases',
+    'author': 'Humanytek',
+    'website': "http://www.humanytek.com",
+    'license': 'AGPL-3',
+    'depends': ['purchase', 'purchase_order_date_receipt_supplier'],
+    'data': [
+        'views/res_company_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
---
+++ models/res_company.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from openerp import fields, models
+
+
+class Company(models.Model):
+    _inherit = 'res.company'
+
+    purchase_order_expiration_limit = fields.Integer(
+        string=u'Limit of days to the expiration of purchase orders',
+        default=30)
